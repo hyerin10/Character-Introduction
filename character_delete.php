9f688f8165513bb1a41547eefc66f1126358166b
local hyerin <dev.hr.kim@gmail.com>
2023-03-03 23:27:34 +0900

    Feat: character data delete

---
+++ character_delete.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<?php
+  $num = $_POST["num"];
+
+  $con = mysqli_connect("localhost", "user1", "12345", "sample");
+  $sql = "delete from character_information where num='$num'";
+  mysqli_query($con, $sql);
+  mysqli_close($con);
+  
+  echo "
+    <script>
+      location.href = 'character_page.php?num=1';
+    </script>
+  ";
+?>
